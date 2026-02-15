b4fe1e8f50a6d0dd76bb117ce90f9710bd2b45bc
Speedylo <86981199+Speedylo@users.noreply.github.com>
2026-02-15 20:27:26 +0000

    Environment separation for dev/test/prod

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-require('dotenv').config()
+require('dotenv-flow').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
 mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on("error", (error) => console.error(error))
-db.once("open", () => console.log("Connected to Database"))
+  .then(() => console.log(`Connected to ${process.env.NODE_ENV} Database`))
+  .catch(console.error);
 
 app.use(express.json())
 
