2e7ecf74118c8c1bde71ab1f0d7c382f0daf2811
Speedylo <86981199+Speedylo@users.noreply.github.com>
2026-02-17 10:43:33 +0000

    More tests for subscribers

--- tests/subscribers.test.js
+++ tests/subscribers.test.js
@@ -31,8 +31,19 @@ describe('GET /subscribers/:id', () => {
 	});
 });
 
-describe('POST /subscribers and DELETE /subscribers/:id', () => {
-	test('create and delete a subscriber', async () => {
+describe('GET /subscribers/:id - FAILURE CASE', () => {
+	test('returns code 404 for non-existing subscriber', async () => {
+		const fakeId = new mongoose.Types.ObjectId();
+
+		const res = await request(app).get(`/subscribers/${fakeId}`);
+
+		expect(res.statusCode).toBe(404);
+		expect(res.body).toHaveProperty('message');
+	});
+});
+
+describe('POST /subscribers, GET/subscribers/:id, and DELETE /subscribers/:id', () => {
+	test('create, get and delete a subscriber', async () => {
 		const create = await request(app)
 			.post('/subscribers')
 			.send({
@@ -45,6 +56,26 @@ describe('POST /subscribers and DELETE /subscribers/:id', () => {
 
 		const createdSubscriberId = create.body._id;
 
+		const get = await request(app).get(`/subscribers/${createdSubscriberId}`);
+
+		expect(get.statusCode).toBe(200);
+		expect(get.headers['content-type']).toMatch(/json/);
+		expect(get.body).toHaveProperty('_id');
+		expect(get.body).toHaveProperty('name');
+		expect(get.body).toHaveProperty('subscribedToChannel');
+
+		const patch = await request(app).patch(`/subscribers/${createdSubscriberId}`)
+			.send({
+				name: 'Modified subscriber',
+				subscribedToChannel: 'Modified channel'
+			});
+		
+		expect(patch.statusCode).toBe(200);
+		expect(patch.headers['content-type']).toMatch(/json/);
+		expect(patch.body).toHaveProperty('_id');
+		expect(patch.body).toHaveProperty('name');
+		expect(patch.body).toHaveProperty('subscribedToChannel');
+
 		const res = await request(app).delete(`/subscribers/${createdSubscriberId}`);
 
 		expect(res.statusCode).toBe(200);
@@ -52,8 +83,21 @@ describe('POST /subscribers and DELETE /subscribers/:id', () => {
 	});
 });
 
-describe('PATCH /subscribers/:id', () => {
-	test('fail to modify inexistent subscriber', async () => {
+describe('POST /subscribers - FAILURE CASE', () => {
+	test('returns code 400 and error message', async () => {
+		const create = await request(app)
+			.post('/subscribers')
+			.send({
+				name: 'Test user'
+			});
+		
+		expect(create.statusCode).toBe(400);
+		expect(create.body).toHaveProperty('message');
+	});
+});
+
+describe('PATCH /subscribers/:id - FAILURE CASE', () => {
+	test('returns code 404 and error message', async () => {
 		const fakeId = new mongoose.Types.ObjectId();
 
 		const res = await request(app).patch(`/subscribers/${fakeId}`)
